b7ede5748d18114a34bb36cebbc2600a09849000
Marianna <mckd2018@gmail.com>
2019-01-09 12:31:39 -0800

    add view controller and README update

---
+++ README.md
@@ -0,0 +1,23 @@
+# Reviewed By Trucker
+
+This app gets information from a trucker about a load to the dispatcher to help maintain on-time delivery.
+**_Reviewed By Trucker_**  An application created with Node, Express, HTML, and CSS.
+
+A trucker will be able to answer and few questions about the load.
+
+## Base URL
+
+```
+http://localhost:3000/
+```
+
+## Running Locally
+Make sure you have [Node.js](http://nodejs.org/) >= 10.11.0 installed.
+
+```sh
+git clone https://github.com/campbellmarianna/Reviewed-By-Trucker.git
+cd reviewed-by-trucker
+npm i && nodemon
+```
+
+The app should now be running on [localhost:3000](http://localhost:3000/).
--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 // Express - defines routes
 const express = require('express')
 var exphbs = require('express-handlebars');
-
+const path = require('path');
 const app = express()
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: "hbs"}));
+app.set('view engine', 'hbs');
+// static files middleware
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
+// OUR MOCK ARRAY OF LOAD INFO
+let loadReview = [
+    { thirdPartyLoadInfo: Object, loadNum: "02467", waitTime: "30", loadWeight: "200", safety: "No Lights", otherComments: "None" },
+    { thirdPartyLoadInfo: Object, loadNum: "26510", waitTime: "50", loadWeight: "600", safety: "Lights", otherComments: "I like this."}
+]
 
 // ROOT ROUTE
 app.get('/', (req, res) => {
-    res.render('home', {msg: 'Handlebars are Cool!'})
+    res.render('review-form');
 })
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
