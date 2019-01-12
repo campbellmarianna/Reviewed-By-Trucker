17b47f3a2424156a85d92168a65bd3e798583deb
Marianna <mckd2018@gmail.com>
2019-01-11 21:19:50 -0800

    add loads index

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // Express - defines routes
-const express = require('express')
+const express = require('express');
 var exphbs = require('express-handlebars');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: "hbs"}));
 app.set('view engine', 'hbs');
 // static files middleware
-app.use('/public', express.static(path.join(__dirname, 'public')))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Point this to production mongodb datatbase URI
 const port = process.env.PORT || 3000;
@@ -30,25 +30,48 @@ const loadReview = mongoose.model('loadReview', {
     lights: String,
     waitReason: String,
     otherComments: String
-})
-
-// OUR MOCK ARRAY OF LOAD INFO
-// let loadReview = [
-//     { thirdPartyLoadInfo: Object, loadNum: "02467", waitTime: "30", loadWeight: "200", safety: "No Lights", otherComments: "None" },
-//     { thirdPartyLoadInfo: Object, loadNum: "26510", waitTime: "50", loadWeight: "600", safety: "Lights", otherComments: "I like this."}
-// ]
+});
 
 // ROOT ROUTE
 app.get('/', (req, res) => {
     res.render('review-form');
-})
+});
 
 // CREATE
-app.post('/review', (req,res) => {
-    console.log(req.body);
+app.post('/success', (req,res) => {
+    loadReview.create(req.body).then((review) => {
+        res.render('success-page')
+    }).catch((err) => {
+        // catch errors
+        console.log(err.message);
+    });
+});
+
+// SHOW Success Form
+app.get('/success', (req, res) => {
+    res.render('success-page');
 })
 
 
+// OUR MOCK ARRAY OF LOAD INFO
+let loads = [
+    { pickupDate: "01/18/19", originLocation: "246 McAllister Street San Fransico, California", destination: "62169 Addison Drive Joplin, Missiouri", price: "2000", content: "sand"},
+    { pickupDate: "01/17/19", originLocation: "555 Post Street San Fransico, California", destination: "54621 Adobe Street Directory, Maine", price: "1800", content: "soap"}
+];
+
+// Load Routes
+// INDEX
+app.get('/loads', (req, res) => {
+    res.render('loads-index', {loads : loads})
+})
 app.listen(port, () => {
     console.log('App listening on port 3000!')
+});
+
+// SHOW
+app.get('/loads/:id', (req, res) => {
+    var loadId = req.params.id;
+    var load = loads[loadId]
+    // var parsed = JSON.parse(req.body)[loadId];
+    res.render('loads-show', { loads : load});
 })
